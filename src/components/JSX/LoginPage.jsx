0c29292c38469cd05edc1b1c5b1c89f88dde5776
IdkFranco <francofredy962@gmail.com>
2025-04-21 00:18:18 -0300

    Actualicé el login con mensajes de validación

--- src/components/JSX/LoginPage.jsx
+++ src/components/JSX/LoginPage.jsx
@@ -1,18 +1,55 @@
 import '../CSS/LoginPage.css'
 import { FaLock, FaUser } from 'react-icons/fa';
+import React, {useState} from 'react';
 
 const LoginPage = () => {
+    const[inputUsername, setInputUsername] = useState('');
+    const[inputPassword, setInputPassword] = useState('');
+    const[allowed, setAllowed] = useState(false);
+    const[error, setError] = useState(false);
+
+    const users = [
+        {username: 'admin', password: '1234'} 
+    ]
+    
+
+    const handleLogin = () => {
+        const foundUser = users.find(
+            (user) => 
+                user.username === inputUsername && user.password === inputPassword
+        );
+
+        if(foundUser) {
+            setAllowed(true);
+            setError(false)
+        } else {
+            setError(true);
+            setAllowed(false);
+        }
+
+    }
+
     return (
         <div className='wrapper'>
             <div className='form-login-box'>
                 <form action=''>
                     <h1>Login</h1>
                     <div className='input-box'>
-                        <input type='text' placeholder='Username'/>
+                        <input 
+                            type='text' 
+                            placeholder='Username'
+                            value={inputUsername}
+                            onChange={(e) => setInputUsername(e.target.value)}
+                        />
                         <FaUser className='icon'/>
                     </div>
                     <div className='input-box'>
-                        <input type='text' placeholder='Password'/>
+                        <input 
+                            type='text' 
+                            placeholder='Password'
+                            value={inputPassword}
+                            onChange={(e) => setInputPassword(e.target.value)}
+                        />
                         <FaLock className='icon'/>
                     </div>
                     <div className='remember-forgot'>
@@ -20,13 +57,18 @@ const LoginPage = () => {
                         <a href='/#'>Forgot password?</a>
                     </div>
                     <div className='button-box'>
-                        <button>
+                        <button type='button' onClick={handleLogin}>
                             login
                         </button>
+                    </div>
+                    <div className='message-box'>
+                        {allowed && <p>Welcome</p>}
+                        {error && <p>Error, try again</p>}
                     </div>
                     <div className="register-link">
                         <p>Don't have an account? <a href='/#'>Register</a></p>
                     </div>
+
                 </form>
 
             </div>
